2020929ac695ab997323403a376c30d80156e08c
Chandana <rebbachandana54@gmail.com>
2026-03-10 16:44:23 +0000

    s

---
+++ var.sh
@@ -0,0 +1,6 @@
+nam1=sindhu
+nam2=bindhu
+
+echo "$nam1: Hi $nam2 , how is your vacation?"
+echo "$nam2: hello $nam1, it went very well....i enjoyed a lot and explored many places ,tried different strret foods and captured lot of memories , i am waiting eagerly for next vacation"
+echo "$nam1: its so good to hear it from you"
