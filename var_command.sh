aadc7bf22430c9d850326ffb09e230da7e95f791
Chandana <rebbachandana54@gmail.com>
2026-03-10 17:20:17 +0000

    s

--- var_command.sh
+++ var_command.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-date_name=$(date +%s)
-echo "script execution timestamp:$date_name"
+# date_name=$(date +%s)
+# echo "script execution timestamp:$date_name"
+start_time=$(date +%s)
+sleep 15
+end_time=$(date +%s)
+total_time=$(($end_time-$start_time))
+echo "script execution completed in $total_time seconds"
