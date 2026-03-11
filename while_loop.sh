9e69a9477be771f1c191349242f0f2c0606bab1e
Chandana <rebbachandana54@gmail.com>
2026-03-11 15:40:04 +0000

    s

--- while_loop.sh
+++ while_loop.sh
@@ -1,6 +1,12 @@
-number=1
-while [ $number -le 5 ]
+# number=1
+# while [ $number -le 5 ]
+#     do
+#         echo $number
+#         ((number++))
+#     done;
+
+$file_path=/home/ec2-user/file.txt
+while read line
     do
-        echo $number
-        ((number++))
-    done;
+        echo $line
+    done < $file_path
