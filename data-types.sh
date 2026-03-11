a7146a1a266b757a521c9c2e9f8fb952502ca04f
Chandana <rebbachandana54@gmail.com>
2026-03-11 12:35:08 +0000

    s

--- data-types.sh
+++ data-types.sh
@@ -1,6 +1,11 @@
-Number1=10
-Number2=20
-Name=Devops
+# Number1=10
+# Number2=20
+# Name=Devops
 
-sum=$(($Number1+$Number2+$Name))
-echo "sum: ${sum}"
+# sum=$(($Number1+$Number2+$Name))
+# echo "sum: ${sum}"
+
+Names= ("shyama" "mahalsapathi" "dasaganu")
+echo "all names: ${Names[@]}"
+echo "first name : ${Names[0]}"
+echo "last name: ${Names[3]}
