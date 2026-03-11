6cfd2a0895d2406e47525ea38fdb668d21f676a4
Chandana <rebbachandana54@gmail.com>
2026-03-11 17:03:41 +0000

    s

--- arrays.sh
+++ arrays.sh
@@ -1,5 +1,11 @@
-servers=("server1" "server2" "server3")
-for s in "${servers[@]}"
-    do 
-        echo  $s
-    done;
+# servers=("server1" "server2" "server3")
+# for s in "${servers[@]}"
+#     do 
+#         echo  $s
+#     done;
+
+declare -A user
+user[name]="Keerthi"
+user[role]="devops"
+echo ${user[name]}
+echo ${user[role]}
