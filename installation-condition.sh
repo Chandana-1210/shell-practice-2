00c902f47551020337a0ca523881eeb4281a0118
Chandana <rebbachandana54@gmail.com>
2026-03-11 13:54:41 +0000

    s

---
+++ installation-condition.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+user_id=$(id -u)
+
+if [ $user_id -ne 0 ]; then 
+    echo "Error: Please run the script with root priveleges"
+fi
+
+dnf install mysql-server
+
+if [ $? -ne 0 ]; then 
+    echo "Error: Installing MYSQL is failure"
+else
+    echo "Installing MYSQL is SUCCESS"
+fi
+ 
